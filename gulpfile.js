6a87f703263b3a17db4c34bd310d2955f6e050d9
Kekelberi <alexei.gaponov@gmail.com>
2022-01-20 18:48:07 +0300

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
-// import { src, dest } from 'gulp'
-const { src, dest } = require('gulp')
+const {src, dest, watch } = require('gulp')
+
+const fc = require('gulp-file-include')
+const min = require('gulp-htmlmin')
+const size = require('gulp-size');
 
 const html = () => {
   return src('./src/html/*.*')
+  .pipe(fc())
+  .pipe(size())
+  .pipe(min({
+    collapseWhitespace: true
+  }))
+  .pipe(size())
   .pipe(dest('./public'))
 }
+
+// watch
+const watcher = () => {
+  watch('./src/html/**/*.html', html)
+}
+
+
 exports.html = html
+exports.watch = watcher
