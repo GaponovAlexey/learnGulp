c34b0206f28805f8a8ea8c2f8b5d11694980de81
Kekelberi <alexei.gaponov@gmail.com>
2022-01-20 19:29:53 +0300

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,38 @@
-const {src, dest, watch } = require('gulp')
+const { src, dest, watch, series, parallel } = require('gulp')
+
+const browSync = require('browser-sync').create()
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
 
 const fc = require('gulp-file-include')
 const min = require('gulp-htmlmin')
-const size = require('gulp-size');
+const size = require('gulp-size')
 
 const html = () => {
   return src('./src/html/*.*')
-  .pipe(fc())
-  .pipe(size())
-  .pipe(min({
-    collapseWhitespace: true
-  }))
-  .pipe(size())
-  .pipe(dest('./public'))
+    .pipe(plumber({
+      errorHandler: notify.onError()
+    }))
+    .pipe(fc())
+    .pipe(size())
+    .pipe(
+      min({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(size())
+    .pipe(dest('./public'))
+    .pipe(browSync.stream())
+}
+
+//server
+
+const server = () => {
+  browSync.init({
+    server: {
+      baseDir: './public',
+    },
+  })
 }
 
 // watch
@@ -20,6 +40,7 @@ const watcher = () => {
   watch('./src/html/**/*.html', html)
 }
 
-
 exports.html = html
 exports.watch = watcher
+
+exports.dev = series(html, parallel(watcher, server))
